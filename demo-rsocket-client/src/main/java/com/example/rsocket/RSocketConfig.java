6062e40e4b3ab9a7851ad3f367eeea252596fe7d
Toshiaki Maki <tmaki@pivotal.io>
2019-04-29 22:11:39 +0900

    Change to WebSocket version

--- demo-rsocket-client/src/main/java/com/example/rsocket/RSocketConfig.java
+++ demo-rsocket-client/src/main/java/com/example/rsocket/RSocketConfig.java
@@ -2,11 +2,13 @@ package com.example.rsocket;
 
 import io.rsocket.frame.decoder.PayloadDecoder;
 import io.rsocket.transport.ClientTransport;
-import io.rsocket.transport.netty.client.TcpClientTransport;
+import io.rsocket.transport.netty.client.WebsocketClientTransport;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
 
 @ConfigurationProperties(prefix = "producer")
 public class RSocketConfig {
@@ -15,14 +17,18 @@ public class RSocketConfig {
 
     private final int port;
 
-    public RSocketConfig(String host, int port) {
+    private final boolean wiretap;
+
+    public RSocketConfig(String host, int port, boolean wiretap) {
         this.host = host;
         this.port = port;
+        this.wiretap = wiretap;
     }
 
     @Bean
     public ClientTransport clientTransport() {
-        return TcpClientTransport.create(this.host, this.port);
+        return WebsocketClientTransport
+            .create(HttpClient.from(TcpClient.create().host(this.host).port(this.port)).wiretap(this.wiretap), "/rsocket");
     }
 
     @Bean
