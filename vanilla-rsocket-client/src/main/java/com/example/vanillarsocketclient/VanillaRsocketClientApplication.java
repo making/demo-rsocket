c96feb28b8cafdf05a4ba2da2e0c2cbb65d253ef
Toshiaki Maki <tmaki@pivotal.io>
2019-04-29 22:13:13 +0900

    Add vanilla RSocket client

---
+++ vanilla-rsocket-client/src/main/java/com/example/vanillarsocketclient/VanillaRsocketClientApplication.java
@@ -0,0 +1,56 @@
+package com.example.vanillarsocketclient;
+
+import io.rsocket.Payload;
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.transport.ClientTransport;
+import io.rsocket.transport.netty.client.WebsocketClientTransport;
+import io.rsocket.util.DefaultPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
+
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+
+public class VanillaRsocketClientApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(VanillaRsocketClientApplication.class);
+
+    public static void main(String[] args) throws Exception {
+        int port = Optional.ofNullable(System.getenv("PORT")).map(Integer::parseInt)
+            .orElse(7000);
+        CountDownLatch latch = new CountDownLatch(1);
+        ClientTransport transport = WebsocketClientTransport.create(HttpClient.from(TcpClient.create()
+            .host("localhost")
+            .port(port).wiretap(true)), "/rsocket");
+        RSocket rsocket = RSocketFactory.connect()
+            .dataMimeType("application/json")
+            .metadataMimeType("text/plain")
+            .transport(transport)
+            .start()
+            .block();
+
+        Mono<String> hello = rsocket.requestResponse(DefaultPayload.create("{\"name\":\"Jane Doe\"}", "hello"))
+            .map(Payload::getDataUtf8)
+            .log("hello");
+
+        Flux<String> datetime = rsocket.requestStream(DefaultPayload.create("{\"zoneId\":\"Asia/Tokyo\"}", "datetime")) //
+            .map(Payload::getDataUtf8) //
+            .log("datetime");
+
+        hello.and(datetime).subscribe();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            latch.countDown();
+            VanillaRsocketClientApplication.log.info("Shutdown");
+        }));
+        latch.await();
+        rsocket.dispose();
+        log.info("Bye");
+    }
+
+}
---
+++ vanilla-rsocket-client/src/test/java/com/example/vanillarsocketclient/VanillaRsocketClientApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.vanillarsocketclient;
+
+import org.junit.Test;
+
+public class VanillaRsocketClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
