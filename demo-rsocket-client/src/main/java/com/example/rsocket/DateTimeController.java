974d6c373353051660f69efd78685536a58aea25
Toshiaki Maki <tmaki@pivotal.io>
2019-11-05 13:07:54 +0900

    Update

--- demo-rsocket-client/src/main/java/com/example/rsocket/DateTimeController.java
+++ demo-rsocket-client/src/main/java/com/example/rsocket/DateTimeController.java
@@ -7,25 +7,22 @@ import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
 @RestController
 public class DateTimeController {
 
-    private final RSocketRequester.Builder rsocketRequesterBuilder;
+    private final Mono<RSocketRequester> requesterMono;
 
-    private final ClientTransport clientTransport;
-
-    public DateTimeController(RSocketRequester.Builder rsocketRequesterBuilder, ClientTransport clientTransport) {
-        this.rsocketRequesterBuilder = rsocketRequesterBuilder;
-        this.clientTransport = clientTransport;
+    public DateTimeController(RSocketRequester.Builder builder, ClientTransport clientTransport) {
+        this.requesterMono = builder.connect(clientTransport);
     }
 
     @GetMapping(path = "/", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<JsonNode> datetime() {
-        return this.rsocketRequesterBuilder
-            .connect(this.clientTransport)
+        return this.requesterMono
             .flatMapMany(requester -> requester.route("datetime")
                 .retrieveFlux(JsonNode.class))
             .retryBackoff(Long.MAX_VALUE, Duration.ofSeconds(10), Duration.ofMinutes(10))
