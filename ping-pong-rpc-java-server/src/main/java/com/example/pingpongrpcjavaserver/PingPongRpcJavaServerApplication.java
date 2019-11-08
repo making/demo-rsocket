9bf0b497f9d3aeee14f061343a4db956b7b5b8a3
Toshiaki Maki <tmaki@pivotal.io>
2019-11-08 23:48:29 +0900

    Update

--- ping-pong-rpc-java-server/src/main/java/com/example/pingpongrpcjavaserver/PingPongRpcJavaServerApplication.java
+++ ping-pong-rpc-java-server/src/main/java/com/example/pingpongrpcjavaserver/PingPongRpcJavaServerApplication.java
@@ -1,5 +1,6 @@
 package com.example.pingpongrpcjavaserver;
 
+import com.example.pingpong.PingPongService;
 import com.example.pingpong.PingPongServiceServer;
 import io.rsocket.RSocketFactory;
 import io.rsocket.rpc.rsocket.RequestHandlingRSocket;
@@ -14,7 +15,8 @@ public class PingPongRpcJavaServerApplication {
 
     public static void main(String[] args) throws Exception {
         final CountDownLatch latch = new CountDownLatch(1);
-        final PingPongServiceServer server = new PingPongServiceServer(new PingPongServiceImpl(), Optional.empty(), Optional.empty());
+        final PingPongService pingPongService = new PingPongServiceImpl();
+        final PingPongServiceServer server = new PingPongServiceServer(pingPongService, Optional.empty(), Optional.empty());
         final Disposable disposable = RSocketFactory.receive()
             .acceptor((setup, sendingSocket) -> Mono.just(new RequestHandlingRSocket(server)))
             .transport(TcpServerTransport.create(9999))
